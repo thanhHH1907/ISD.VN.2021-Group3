f9411d960133e5d15e0aff7afa31c2a2e28ceba2
thanhHH1907 <67831088+thanhHH1907@users.noreply.github.com>
2021-12-28 14:55:15 +0700

    commit

---
+++ Lab08/ViewStationController.java
@@ -0,0 +1,11 @@
+package org.hust.controller;
+
+public class ViewStationController extends BaseController {
+    public boolean validateId(String id) {
+        return true;
+    }
+
+    public boolean validateLocation(String location) {
+        return true;
+    }
+}
