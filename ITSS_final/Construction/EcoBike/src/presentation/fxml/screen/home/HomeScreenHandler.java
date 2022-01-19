2c649e0f72f71aab9257f28de6efeef57b85657c
thanhHH1907 <67831088+thanhHH1907@users.noreply.github.com>
2022-01-19 10:42:02 +0700

    commit

---
+++ ITSS_final/Construction/EcoBike/src/presentation/fxml/screen/home/HomeScreenHandler.java
@@ -0,0 +1,246 @@
+package views.screen.home;
+
+import controller.HomeController;
+import controller.RentBikeController;
+import controller.ViewBikeController;
+import controller.SelectDockToReturnBikeController;
+import entity.order.Order;
+import entity.station.Station;
+//import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import utils.Configs;
+import utils.Utils;
+import views.screen.BaseScreenHandler;
+import views.screen.barcode.BarcodeScreenHandler;
+import views.screen.returnbike.SelectDockToReturnBikeScreenHandler;
+import views.screen.bike.RentingBikeHandler;
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+import java.util.logging.Logger;
+/**
+ * This class is to display the home screen
+ * @author Do Minh Thong
+ * @version 1.0
+ */
+
+public class HomeScreenHandler extends BaseScreenHandler implements Initializable {
+
+    public static Logger LOGGER = Utils.getLogger(HomeScreenHandler.class.getName());
+
+    @FXML
+    private ScrollPane scrollPane;
+
+    @FXML
+    private HBox hboxHome;
+
+    @FXML
+    private Label homeLabel;
+
+    @FXML
+    private ImageView logo;
+
+    @FXML
+    private VBox vboxDock1;
+
+    @FXML
+    private VBox vboxDock2;
+
+    @FXML
+    private Button rentBikeButton;
+
+    @FXML
+    private Button viewRent;
+
+    private List<StationHandler> homeItems;
+    @FXML
+    private Button viewRentBike;
+    private Order order;
+
+    /**
+     * constructor, when the user is not using any bike
+     * @param stage
+     * @param screenPath
+     * @throws IOException
+     * @throws SQLException
+     */
+    public HomeScreenHandler(Stage stage, String screenPath) throws IOException, SQLException {
+        super(stage, screenPath);
+        this.order = null;
+        initHome(this.order);
+    }
+    
+    /**
+     * constructor, when the user is using a rented bike
+     * @param stage
+     * @param screenPath
+     * @param order
+     * @throws IOException
+     * @throws SQLException
+     */
+    public HomeScreenHandler(Stage stage, String screenPath, Order order) throws IOException, SQLException {
+        super(stage, screenPath);
+        this.order = order;
+        initHome(order);
+
+        if (order != null) {
+            rentBikeButton.setText("Return Bike");
+            rentBikeButton.setStyle("-fx-background-color: #4d5deb");
+            rentBikeButton.setOnMouseClicked(e -> {
+                SelectDockToReturnBikeScreenHandler selectDock;
+                try {
+                    selectDock = new SelectDockToReturnBikeScreenHandler(this.stage, Configs.SELECT_DOCK_TO_RETURN_BIKE_PATH, order);
+                    selectDock.setHomeScreenHandler(this);
+                    selectDock.setBController(new SelectDockToReturnBikeController());
+                    selectDock.requestToSelectDock(this);
+                } catch (Exception e1) {
+                    e1.printStackTrace();
+                }
+            });
+
+            viewRent.setVisible(true);
+            viewRent.setOnMouseClicked(e -> {
+                RentingBikeHandler viewRentingBike;
+                try {
+                    viewRentingBike = new RentingBikeHandler(this.stage, Configs.RENT_BIKE_INFO_PATH, order);
+                    viewRentingBike.setHomeScreenHandler(this);
+                    viewRentingBike.setBController(new ViewBikeController());
+                    viewRentingBike.requestToViewRentingBike(this);
+                } catch (Exception e1) {
+                    e1.printStackTrace();
+                }
+            });
+        }
+    }
+
+    /**
+     * get the controller of this screen
+     */
+    public HomeController getBController() {
+        return (HomeController) super.getBController();
+    }
+
+
+    @Override
+    /**
+     * initialize the screen
+     */
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        setBController(new HomeController());
+        try {
+            rentBikeButton.setOnMouseClicked(e -> {
+                BarcodeScreenHandler barcodeScreen;
+                try {
+                    barcodeScreen = new BarcodeScreenHandler(this.stage, Configs.BARCODE_SCREEN_PATH);
+                    barcodeScreen.setHomeScreenHandler(this);
+                    barcodeScreen.setBController(new RentBikeController());
+                    barcodeScreen.requestToViewBarcode(this);
+                } catch (Exception e1) {
+                    e1.printStackTrace();
+                }
+            });
+            viewRent.setVisible(false);
+        } catch (Exception e) {
+            LOGGER.info("Errors occured: " + e.getMessage());
+            e.printStackTrace();
+        }
+
+        if (order != null) {
+            rentBikeButton.setText("Rent Bike");
+            rentBikeButton.setStyle("-fx-background-color:  #eb4d55");
+            rentBikeButton.setOnMouseClicked(e -> {
+                RentingBikeHandler viewRentingBikeHandler;
+                try {
+                    viewRentingBikeHandler = new RentingBikeHandler(this.stage, Configs.RENT_BIKE_INFO_PATH, order);
+                    viewRentingBikeHandler.setHomeScreenHandler(this);
+                    viewRentingBikeHandler.setBController(new ViewBikeController());
+                    viewRentingBikeHandler.requestToViewRentingBike(this);
+                } catch (Exception e1) {
+                    e1.printStackTrace();
+                }
+            });
+        }
+    }
+    
+    /**
+     * display the home screen
+     * @param searchString
+     * @param order
+     * @throws SQLException
+     * @throws IOException
+     */
+    public void initHome(Order order) throws SQLException, IOException {
+        setBController(new HomeController());
+        List<Station> medium = getBController().getAllStations();
+        this.homeItems = new ArrayList<>();
+        if (order != null) {
+            for (Object object : medium) {
+                Station station = (Station) object;
+                StationHandler dock = new StationHandler(Configs.STATION_HOME_PATH, station, this, order);
+                this.homeItems.add(dock);
+            }
+        } else {
+            for (Object object : medium) {
+                Station station = (Station) object;
+                StationHandler dock = new StationHandler(Configs.STATION_HOME_PATH, station, this);
+                this.homeItems.add(dock);
+            }
+        }
+        addStationHome(this.homeItems);
+    }
+
+    /**
+     * request to move to this screen
+     * @param prevScreen
+     * @throws SQLException
+     */
+    public void requestToReturnHome(BaseScreenHandler prevScreen) throws SQLException {
+        setPreviousScreen(prevScreen);
+        setScreenTitle("Home");
+        show();
+    }
+
+    public void setImage() {
+        return;
+    }
+
+    public Stage getStage() {
+        return this.stage;
+    }
+
+    /**
+     * add station to home
+     * @param items
+     */
+    public void addStationHome(List<StationHandler> items) {
+        ArrayList<StationHandler> homeItems = (ArrayList) ((ArrayList) items).clone();
+        hboxHome.getChildren().forEach(node -> {
+            VBox vBox = (VBox) node;
+            vBox.getChildren().clear();
+        });
+        while (!homeItems.isEmpty()) {
+            int size = homeItems.size();
+            hboxHome.getChildren().forEach(node -> {
+//                int vid = hboxHome.getChildren().indexOf(node);
+                VBox vBox = (VBox) node;
+                vBox.setSpacing(20);
+                while (vBox.getChildren().size() <= size && !homeItems.isEmpty()) {
+                    StationHandler station = (StationHandler) homeItems.get(0);
+                    vBox.getChildren().add(station.getContent());
+                    homeItems.remove(station);
+                }
+            });
+            return;
+        }
+    }
+}
---
+++ ITSS_final/Construction/EcoBike/src/presentation/fxml/screen/returnbike/ReturnBikeHandler.java
@@ -0,0 +1,223 @@
+package views.screen.returnbike;
+
+import controller.BaseController;
+import controller.PaymentController;
+import controller.ReturnBikeController;
+import entity.bike.Bike;
+import entity.invoice.Invoice;
+import entity.order.Order;
+import entity.station.Station;
+import entity.transaction.Card;
+import entity.transaction.TransactionInfo;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import subsystem.interbank.InterbankSubsystemController;
+import utils.Configs;
+import utils.Utils;
+import views.screen.BaseScreenHandler;
+import views.screen.payment.PaymentScreenHandler;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.logging.Logger;
+
+/**
+ * This class is to display the return bike screen
+ * @author Tran Thi Hang
+ * @version 1.0
+ */
+public class ReturnBikeHandler extends BaseScreenHandler {
+
+    @FXML
+    private Text numberPlate;
+    @FXML
+    private Text barcode;
+    @FXML
+    private Text type;
+    @FXML
+    private Text deposit;
+    @FXML
+    private Text rentedTime;
+    @FXML
+    private Text total;
+    @FXML
+    private Text rentingFee;
+    @FXML
+    private Text station;
+    @FXML
+    private Text start;
+    @FXML
+    private Text end;
+    @FXML
+    private Text payType;
+    @FXML
+    private Text errorMessage;
+
+    @FXML
+    private ImageView bikeImage;
+
+    @FXML
+    private TextField owner;
+    @FXML
+    private TextField cardCode;
+
+    @FXML
+    private TextField dateExpired;
+    @FXML
+    private PasswordField cvvCode;
+    @FXML
+    private Button cancelBtn;
+    @FXML
+    private Button submitBtn;
+    @FXML
+    private Button editBtn;
+    @FXML
+    private ImageView logo;
+
+    private Station newStation;
+    private Card card;
+    private Order order;
+    private int totalAmount;
+    private String invoiceContents;
+
+    private static Logger LOGGER = Utils.getLogger(ReturnBikeHandler.class.getName());
+
+    /**
+     * constructor
+     * @param stage
+     * @param screenPath
+     * @param bController
+     * @param station
+     * @param order
+     * @throws IOException
+     */
+    public ReturnBikeHandler(Stage stage, String screenPath, BaseController bController, Station station, Order order) throws IOException {
+        super(stage, screenPath);
+        setBController(bController);
+        this.order = order;
+        this.newStation = station;
+
+        //update rented bike's station
+        this.order.getRentedBike().setStation(newStation);
+        setBikeInfo();
+        setCardInfo();
+        logo.setOnMouseClicked(event -> {
+            try {
+                backToHomeAfterRent(order);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    /**
+     * get the controller of this screen
+     */
+    public ReturnBikeController getBController() {
+        return (ReturnBikeController) super.getBController();
+    }
+
+    /**
+     * set bike info
+     */
+    private void setBikeInfo() {
+        Bike bike = this.order.getRentedBike();
+        numberPlate.setText(bike.getLicensePlate());
+        barcode.setText(bike.getBarcode());
+        type.setText(bike.getType());
+        station.setText(newStation.getName());
+        int deposit1 = getBController().getBikeDeposit(bike);
+        deposit.setText(Utils.getCurrencyFormat(deposit1));
+        setImage(bikeImage, bike.getUrlImage());
+
+        start.setText((order.getStart().format(Utils.DATE_FORMATTER)));
+        end.setText(LocalDateTime.now().format(Utils.DATE_FORMATTER));
+
+        // format rentedTime
+        int hours = Duration.between(order.getStart(), LocalDateTime.now()).toHoursPart();
+        long days = Duration.between(order.getStart(), LocalDateTime.now()).toDaysPart();
+        String rentedTimeText = days > 0 ? days + " days " : "";
+        rentedTimeText += hours > 0 ? hours + " hours " : "";
+        rentedTimeText += Duration.between(order.getStart(), LocalDateTime.now()).toMinutesPart() + " minutes";
+        rentedTime.setText(rentedTimeText);
+
+        int rFee = new ReturnBikeController().calculateAmount(order.getRentedBike().getCoefficient(), order.getStart());
+        getBController().setAmountOrder(order, rFee);
+        rentingFee.setText(Utils.getCurrencyFormat(rFee));
+
+        totalAmount = rFee - deposit1;
+        // pay more if rentingFee > deposit
+        if (totalAmount > 0) {
+            payType.setText("Pay amount");
+            this.invoiceContents = "Pay additional for returning bike";
+            total.setText(Utils.getCurrencyFormat(totalAmount));
+        }
+        // else refund
+        else {
+            payType.setText("ReturnBike");
+            this.invoiceContents = "Refund for returning bike";
+            total.setText(Utils.getCurrencyFormat(-totalAmount));
+        }
+    }
+
+    @FXML
+    /**
+     * move to payment screen
+     * @param event
+     * @throws IOException
+     * @throws SQLException
+     */
+    void moveToPaymentScreen(MouseEvent event) throws IOException, SQLException {
+        Invoice invoice = getBController().createInvoice(order, totalAmount, this.invoiceContents);
+        PaymentScreenHandler payment = new PaymentScreenHandler(this.stage, Configs.PAYMENT_SCREEN_PATH, invoice, this.card);
+        payment.setBController(new PaymentController());
+        payment.requestToPaymentScreen(this, homeScreenHandler);
+    }
+    
+    /**
+     * set the card info display in the current screen
+     */
+    private void setCardInfo() {
+        this.card = getBController().createCard(Configs.CARD_CODE, Configs.OWNER, Configs.CVV, Configs.DATE_EXPIRED);
+        owner.setText(card.getOwner());
+        cardCode.setText(card.getCardCode());
+        dateExpired.setText(card.getDateExpired());
+    }
+
+    @FXML
+    /**
+     * handle the process when user click submit to return bike
+     * @param event
+     * @throws IOException
+     * @throws SQLException
+     */
+    void submitReturnBike(MouseEvent event) throws IOException, SQLException {
+
+        getBController().setCvvCode(cvvCode.getText(), card);
+        try {
+            getBController().validateCard(card.getCardCode(), card.getOwner(), card.getCvvCode(), card.getDateExpired());
+            getBController().processReturnBike(card, totalAmount, order, invoiceContents, this.stage, homeScreenHandler, this);
+        } catch (Exception e) {
+            errorMessage.setText("* You have to fill in security code");
+        }
+    }
+
+    /**
+     * back to select dock screen
+     * @param event
+     * @throws IOException
+     */
+    @FXML
+    void backToDockSelection(MouseEvent event) throws IOException {
+        SelectDockToReturnBikeScreenHandler d = new SelectDockToReturnBikeScreenHandler(stage, Configs.SELECT_DOCK_TO_RETURN_BIKE_PATH, order);
+        d.show();
+    }
+}
